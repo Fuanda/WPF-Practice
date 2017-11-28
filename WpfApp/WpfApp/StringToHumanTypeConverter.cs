7943dd085aa83bc676961bb0e3b8b2f41fadfd5f
Moment <185036583@qq.com>
2017-11-28 11:44:52 +0800

    test

---
+++ WpfApp/WpfApp/StringToHumanTypeConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp
+{
+    public class StringToHumanTypeConverter : TypeConverter
+    {
+        public override object ConvertFrom(ITypeDescriptorContext context, CultureInfo culture, object value)
+        {
+            if (value is string)
+            {
+                Human h = new Human
+                {
+                    Name = value as string
+                };
+                return h;
+            }
+            return base.ConvertFrom(context, culture, value);
+        }
+    }
+}
+
